287e2d6d8c24a4cef6da92b6d35cd699954346a1
Ancevt <me@ancevt.com>
2023-04-12 20:33:30 +0300

    Initial commit

---
+++ notice.md
@@ -0,0 +1,3 @@
+Excluder (file/directory excluder)
+Copyright (C) 2022 Ancevt (me@ancevt.com)
+
---
+++ src/main/java/com/ancevt/excluder/Excluder.java
@@ -0,0 +1,10 @@
+package com.ancevt.excluder;
+
+import com.ancevt.localstorage.LocalStorage;
+
+public class Excluder {
+
+    public static void main(String[] args) {
+
+    }
+}
